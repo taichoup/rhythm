a9e5bd00a7ed42c3feb6e5a9a123886ba6d58bfe
Manuel Dalle <manuel.dalle@dashlane.com>
2020-04-21 15:27:20 +0200

    reactify class edits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [bpm, setBpm] = useState(60);
+  const [isRunning, setIsRunning] = useState(false);
 
   const handleUserInput = (event) => {
     setBpm(event.target.value);
@@ -16,24 +17,27 @@ function App() {
 
   // todo: rewrite this in a reacty way
   function toggleRotate(speed) {
+    setIsRunning(!isRunning);
     let c = document.getElementById("circle");
     let b = document.getElementById("btn");
-    c.classList.length === 0 ? c.classList.add("circle-rotate") : c.classList = "";
     b.innerText === "Start!" ? b.innerText = "Stop" : b.innerText = "Start!";
-    console.log(c.style.animationDuration);
   }
 
   return (
     <div className="App">
       <div id="bpm-wrapper">
         <input type="range" id="bpm" name="bpm" min="60" max="240" step="1" onChange={handleUserInput} onMouseUp={updateSpeed}/>
-        <span class="bpm">{`${bpm} BPM`}</span>
+        <span className="bpm">{`${bpm} BPM`}</span>
       </div>
     <div id="wrapper">
           <div id="path"></div>
-          <div id="circle"></div>
+          <div id="circle" className={isRunning ? "circle-rotate" : ""}></div>
         </div>
-        <button id="btn" class="btn btn-primary" onClick={() => toggleRotate()}>Start!</button>
+        <button 
+          id="btn"
+          className={`btn ${isRunning ? "btn-warning" : "btn-primary"}`}
+          onClick={() => toggleRotate()}
+        >{isRunning ? "Stop" : "Start!"}</button>
     </div>
   );
 }
